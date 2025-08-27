a4c42c0b4c480b695e5c4d8d68cc44e0d9d8e26c
Christian Schüler <cs.schueler@gmail.com>
2025-08-27 18:02:48 +0300

    DECLARE packages

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	p "prisma"
+)
+
+func main() {
+	fmt.Println(p.ReturnResult())
+}
---
+++ prisma.go
@@ -0,0 +1,15 @@
+package prisma
+
+func connect() {
+	// connect to the postgres database given in the .env
+	return
+}
+
+func disconnect() {
+	// disconnect from the database
+	return
+}
+
+func ReturnResult() {
+	return
+}
